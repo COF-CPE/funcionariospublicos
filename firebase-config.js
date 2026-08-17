3938e985dc0e279378c88c2deefc09283b003605
COF-CPE <aokuma@iadb.org>
2026-08-17 11:27:25 -0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// Reemplaza estos valores con los de tu proyecto Firebase
-// (Configuración del proyecto > Tus apps > SDK setup and configuration)
 export const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyCm6KgrFZNV2PmUUGuWT5CbIzOf-lCkIew",
+  authDomain: "cofcpe.firebaseapp.com",
+  projectId: "cofcpe",
+  storageBucket: "cofcpe.firebasestorage.app",
+  messagingSenderId: "672586227760",
+  appId: "1:672586227760:web:87eeb7ad58c09e6ebdcdf5",
+  measurementId: "G-QBYNXVXVG9",
 };
